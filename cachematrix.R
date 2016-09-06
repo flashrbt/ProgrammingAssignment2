52632f8742927ddba8732f41879391ed710cb113
flashrbt2015@gmail.com <flashrbt2015@gmail.com>
2016-09-06 16:37:41 -0700

    First version of the cachematrix.R tested, works well. will add comments soon.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+    Inver<-NULL
+    set<-function(y){
+        
+        x<<-y
+        Inver<<-NULL
+    }
+    get<-function() x
+    setInverse<-function(Inverse) Inver<<-Inverse
+    getInverse<-function() Inver
+    list(set=set,
+         get=get,
+         setInverse=setInverse,
+         getInverse=getInverse)
 
 }
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    Inver<-x$getInverse()
+    if(!is.null(Inver)){
+            message('getting cached inverse of Matrix')
+            return(Inver)
+    }
+    data<-x$get()
+    Inver<-solve(data,...)
+    x$setInverse(Inver)
+    Inver
 }
