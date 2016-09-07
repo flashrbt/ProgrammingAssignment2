2ba8a45f341303a50c3a4e6bade02d13b303546f
flashrbt2015@gmail.com <flashrbt2015@gmail.com>
2016-09-06 20:01:46 -0700

    Added more comments for two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Date: 09/06/2016 
+##Authoer: CQ Li
+##This program has two functions: makeCacheMatrix() and cacheSolve(). 
+##      makeCacheMatrix() creates a matrix with four methods:
+##          1. set, to set Matrix value
+##          2. get, to reture matrix value
+##          3. setInverse, change the inverse of matrix value
+##          4. getInverse, reture the inverse of matrix value
+##      cacheSolve() reture the inverse of matrix value, if it is null, it will calculte the inverse of matrix value, save it and return the value back. 
 
 ## Write a short comment describing this function
+## 
+## makeCacheMatrix() defines four methods for matrix. To use this function, simple call makeCacheMatrix() and return a matrix with four methods. another way to use it is assign a matrix using set method. 
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -10,10 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
         
         x<<-y
         Inver<<-NULL
+        
     }
+    
     get<-function() x
     setInverse<-function(Inverse) Inver<<-Inverse
     getInverse<-function() Inver
+    
     list(set=set,
          get=get,
          setInverse=setInverse,
@@ -22,7 +34,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() return the inverse of matrix. if the inverse value is cached, it only return this cached value, otherwise, this fuction call solve(x) to caculte the inverse value and save this value using setInverse() method.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
